e6f3addb087e0e92ff2b52a5dffa26eae8744abd
genyrosk <e.roskach@gmail.com>
2021-10-26 20:16:34 +0900

    style: format using prettier

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { ApolloClient, ApolloProvider, createHttpLink, InMemoryCache } from '@apollo/client';
-import { AudioPlayer } from 'kidsloop-audio-player';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import reportWebVitals from './reportWebVitals'
+import {
+  ApolloClient,
+  ApolloProvider,
+  createHttpLink,
+  InMemoryCache,
+} from '@apollo/client'
+import { AudioPlayer } from 'kidsloop-audio-player'
 
 const link = createHttpLink({
-  uri: "/audio-storage/graphql",
-  credentials: "include",
-});
+  uri: '/audio-storage/graphql',
+  credentials: 'include',
+})
 const client = new ApolloClient({
   link,
   cache: new InMemoryCache(),
-});
+})
 
 ReactDOM.render(
   <ApolloProvider client={client}>
@@ -22,10 +27,10 @@ ReactDOM.render(
       mimeType="audio/webm"
     />
   </ApolloProvider>,
-  document.getElementById("root")
-);
+  document.getElementById('root'),
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
